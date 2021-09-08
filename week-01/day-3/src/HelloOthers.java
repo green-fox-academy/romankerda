4497e06c800481a39faddd8eac483674891880cf
Roman Kerda <roman.kerda@gmail.com>
2021-09-08 13:19:17 +0200

    add excrcises Printing to the terminal window, add .DS_Store to .gitignore

---
+++ week-01/day-3/src/HelloOthers.java
@@ -0,0 +1,14 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        // Greet 3 of your classmates with this program in three separate lines
+        // like:
+        //
+        // Hello, Esther!
+        // Hello, Mary!
+        // Hello, Joe!
+
+        System.out.println("Hello Jiri!");
+        System.out.println("Hello Patrik!");
+        System.out.println("Hello Jozef!");
+    }
+}
