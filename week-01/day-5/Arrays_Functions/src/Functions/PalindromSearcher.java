11e0efa941b84994ae22a39b7e8125be4d151773
Roman Kerda <roman.kerda@gmail.com>
2021-09-11 09:51:21 +0200

    clean code

--- week-01/day-5/Arrays_Functions/src/Functions/PalindromSearcher.java
+++ week-01/day-5/Arrays_Functions/src/Functions/PalindromSearcher.java
@@ -10,6 +10,8 @@ public class PalindromSearcher {
     // and return a list with the found palindromes.
 
     System.out.println(Arrays.toString(searchPalindrome("dog goat dad duck doodle never")));
+    System.out.println(Arrays.toString(searchPalindrome("racecar")));
+    System.out.println(Arrays.toString(searchPalindrome("now try czech word kajak, to see if it works")));
 
   }
 
@@ -21,9 +23,9 @@ public class PalindromSearcher {
       size += count;
       count++;
     }
-    String[] posStrings = new String[size];
+    String[] subStrings = new String[size];
 
-    //fill new string[] with all possible words (>=3), substring(ind, ind);
+    //fill new string[]  with all possible words (>=3), substring(ind, ind);
     int wordLength;
     int pos = 0;
 
@@ -31,28 +33,28 @@ public class PalindromSearcher {
       wordLength = input.length() - i;
       if (wordLength >= 3) {
         for (int j = 0; j < input.length() - wordLength + 1; j++) {
-          posStrings[pos] = input.substring(j, j + wordLength);
+          subStrings[pos] = input.substring(j, j + wordLength);
           pos++;
         }
       }
     }
 
-    //iterate new string[] - ifPalindrome, keep it, if not, put "no"; countPalindroms
+    //iterate new string[] - ifPalindrome, keep it, if not, put "no"; count Palindromes
     count = 0;
-    for (int i = 0; i < posStrings.length; i++) {
-      if (isPalindrome(posStrings[i])) {
+    for (int i = 0; i < subStrings.length; i++) {
+      if (isPalindrome(subStrings[i])) {
         count++;
       } else {
-        posStrings[i] = "no";
+        subStrings[i] = "no";
       }
     }
 
     //new clean string, length - countPalidnroms, iterate new string copy palindroms here
     String[] output = new String[count];
     pos = 0;
-    for (int i = 0; i < posStrings.length; i++){
-      if (!posStrings[i].equals("no")){
-        output[pos] = posStrings[i];
+    for (int i = 0; i < subStrings.length; i++){
+      if (!subStrings[i].equals("no")){
+        output[pos] = subStrings[i];
         pos++;
       }
     }
