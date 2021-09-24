2bbad782e24a1124822aa92151e0a3a41001d28c
Roman Kerda <roman.kerda@gmail.com>
2021-09-24 22:30:34 +0200

    update file

--- week-03/day-4/src/Flyable/Helicopter.java
+++ week-03/day-4/src/Flyable/Helicopter.java
@@ -8,16 +8,13 @@ public class Helicopter extends Vehicle implements Flyable {
 
   @Override
   public void land() {
-
   }
 
   @Override
   public void fly() {
-
   }
 
   @Override
   public void takeOff() {
-
   }
 }
