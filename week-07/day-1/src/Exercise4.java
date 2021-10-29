8050fcd1240926e3ef0a23f9e0071c0c19ac9dc9
romankerda <roman.kerda@gmail.com>
2021-10-29 09:12:57 +0200

    correct to get odd numbers, not even

--- week-07/day-1/src/Exercise4.java
+++ week-07/day-1/src/Exercise4.java
@@ -12,7 +12,7 @@ public class Exercise4 {
     List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
 
     numbers.stream()
-            .filter(n -> n % 2 == 0)
+            .filter(n -> n % 2 != 0)
             .mapToInt(n -> n)
             .average()
             .ifPresent(avg -> System.out.printf("Average of odd numbers = %s\n", avg));
